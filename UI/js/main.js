639d8c6bf6c7aede595a30ebd33c16de30cfcf8a
Aime Malaika <aimemalaika1995@gmail.com>
2019-04-19 07:07:03 +0200

    adding registration div and effect to open it on click in the same page login registration and recover password done

--- UI/js/main.js
+++ UI/js/main.js
@@ -18,3 +18,39 @@ function showlogin(){
     login.classList.remove('hidden');
     login.classList.add('animate-left')
 }
+function showregisteronlogin(){
+    var login = document.getElementById('login');
+    login.classList.add('hidden');
+    var recover = document.getElementById('recover');
+    recover.classList.add('hidden');
+    var register = document.getElementById('register');
+    register.classList.remove('hidden');
+    register.classList.add('animate-bottom')
+}
+function showregisteronrecover(){
+    var recover = document.getElementById('recover');
+    recover.classList.add('hidden');
+    var register = document.getElementById('register');
+    register.classList.remove('hidden');
+    register.classList.add('animate-bottom')
+}
+function showrecoveronregister(){
+    var register = document.getElementById('register');
+    register.classList.remove('animate-right');
+    register.classList.add('animate-left');
+    register.classList.remove('animate-left');
+    register.classList.add('hidden');
+    var recover = document.getElementById('recover');
+    recover.classList.remove('hidden');
+    recover.classList.add('animate-left')
+}
+function showloginonregister(){
+    var register = document.getElementById('register');
+    register.classList.remove('animate-right');
+    register.classList.add('animate-left');
+    register.classList.remove('animate-left');
+    register.classList.add('hidden');
+    var login = document.getElementById('login');
+    login.classList.remove('hidden');
+    login.classList.add('animate-left')
+}
